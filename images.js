f17e6bb6f78a5d2289b89f413341d9f97c413c2d
Moiseev Gennadii <genaxyligan@gmail.com>
2026-03-07 19:04:42 +0300

    начало

---
+++ images.js
@@ -0,0 +1,37 @@
+// images.js - данные для галереи
+const galleryImages = {
+  tonirovka: [
+    { files: ['tonirovka1.jpg'], caption: 'Тонировка BMW, пленка 15%' },
+    { files: ['tonirovka2.jpg'], caption: 'Тонировка Audi, хамелеон' }
+  ],
+  himchistka: [
+    { 
+      files: ['himchistka-skoda-do.jpg', 'himchistka-skoda-posle.jpg'], 
+      caption: 'Химчистка салона Skoda до/после' 
+    },
+    { 
+      files: ['photo_2026-02-18_18-01-46.jpg', 'photo_2026-02-18_18-01-58.jpg'], 
+      caption: 'Химчистка салона Mercedes до/после' 
+    },
+    { 
+      files: ['himchistka2.jpg'], 
+      caption: 'Чистка кожаных сидений' 
+    }
+  ],
+  polirovka: [
+    { 
+      files: ['polirovka1-do.jpg', 'polirovka1-posle.jpg'], 
+      caption: 'Полировка фар до/после' 
+    }
+  ],
+  pokraska: [
+    { 
+      files: ['pokraska1-do.jpg', 'pokraska1-posle.jpg'], 
+      caption: 'Покраска бампера до/после' 
+    },
+    { 
+      files: ['pokraska2.jpg'], 
+      caption: 'Локальная покраска крыла' 
+    }
+  ]
+};
