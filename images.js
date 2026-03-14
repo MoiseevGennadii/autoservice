51a568d7035bcad5441794f82b517c0cd829942a
Moiseev Gennadii <genaxyligan@gmail.com>
2026-03-14 15:09:32 +0300

    gallery foto

--- images.js
+++ images.js
@@ -2,30 +2,29 @@
 const galleryImages = {
   tonirovka: [
     { files: ['tonirovka1.jpg'], caption: 'Тонировка BMW, пленка 15%' },
-    { files: ['tonirovka2.jpg'], caption: 'Тонировка Audi, хамелеон' }
   ],
   himchistka: [
-    { 
-      files: ['himchistka-skoda-do.jpg', 'himchistka-skoda-posle.jpg'], 
-      caption: 'Химчистка салона Skoda до/после' 
+    {
+      files: [
+        'photo_2026-02-18_18-01-46.jpg', 'photo_2026-02-18_18-01-58.jpg',
+        'photo_2026-03-14_12-26-57.jpg', 'photo_2026-03-14_12-26-33.jpg'
+      ], caption: 'Химчистка салона до/после'
     },
-    { files: ['photo_2026-02-18_18-01-46.jpg', 'photo_2026-02-18_18-01-58.jpg'], caption: 'Химчистка салона Mercedes до/после' },
-    { files: ['himchistka2.jpg'], caption: 'Чистка кожаных сидений' }
   ],
   polirovka: [
-    { 
-      files: ['polirovka1-do.jpg', 'polirovka1-posle.jpg'], 
-      caption: 'Полировка фар до/после' 
+    {
+      files: ['photo_2026-03-14_12-37-43.jpg', 'photo_2026-03-14_12-27-07.jpg'],
+      caption: 'Полировка фар до/после'
     }
   ],
   pokraska: [
-    { 
-      files: ['pokraska1-do.jpg', 'pokraska1-posle.jpg'], 
-      caption: 'Покраска бампера до/после' 
+    {
+      files: ['pokraska1-do.jpg', 'pokraska1-posle.jpg'],
+      caption: 'Покраска бампера до/после'
     },
-    { 
-      files: ['pokraska2.jpg'], 
-      caption: 'Локальная покраска крыла' 
+    {
+      files: ['pokraska2.jpg'],
+      caption: 'Локальная покраска крыла'
     }
   ]
 };
